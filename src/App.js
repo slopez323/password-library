8b8ac48c4bd315768be01b082c42adeb22ea879c
Sarah Lopez <slopez323@gmail.com>
2025-03-20 14:30:55 -0400

    validate password

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import { CreatePassword } from "./components/create-password";
 // xx clean up files and create relevant components
 // xx page structure - main component (with description); password inputs
 // xx check both inputs matching
-// validate on submit
+// xx validate on submit
+// show invalid reqs
 // some styling
 // accessibility
 // tests
--- src/components/create-password.js
+++ src/components/create-password.js
@@ -5,6 +5,22 @@ export const CreatePassword = () => {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [isMatching, setIsMatching] = useState(true);
+  const [isLengthValid, setIsLengthValid] = useState(false);
+  const [hasUppercase, setHasUppercase] = useState(false);
+  const [hasLowercase, setHasLowercase] = useState(false);
+  const [hasNumber, setHasNumber] = useState(false);
+  const [hasSpecial, setHasSpecial] = useState(false);
+  const isValid =
+    isLengthValid && hasUppercase && hasLowercase && hasNumber && hasSpecial;
+
+  const validatePassword = () => {
+    /* checking that inputs match is already handled by disabling submit button */
+    setIsLengthValid(password.length >= 6);
+    setHasUppercase(/[A-Z]/.test(password));
+    setHasLowercase(/[a-z]/.test(password));
+    setHasNumber(/[0-9]/.test(password));
+    setHasSpecial(/[!@#$%^&*()_+={[}\]|:;"'<,>.-]/.test(password));
+  };
 
   useEffect(() => {
     if (confirmPassword) {
@@ -20,33 +36,44 @@ export const CreatePassword = () => {
 
   return (
     <div>
-      <h1>Create a password</h1>
-      <div>
-        Password must meet the following requirements:
-        <ul>
-          <li>Minimum length of 6 characters</li>
-          <li>At least 1 uppercase character</li>
-          <li>At least 1 lowercase character</li>
-          <li>At least 1 number</li>
-          <li>{`At least 1 special character (!@#$%^&*()_-+={[}]|:;"'<,>.)`}</li>
-        </ul>
-      </div>
-      <form>
-        <PasswordInput
-          name="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <PasswordInput
-          name="confirmPassword"
-          placeholder="Confirm Password"
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-        />
-      </form>
-      {!isMatching && <p>The passwords do not match.</p>}
-      <button disabled={!isMatching || !confirmPassword}>Submit</button>
+      {isValid ? (
+        <h1>Successfully created password!</h1>
+      ) : (
+        <>
+          <h1>Create a password</h1>
+          <div>
+            Password must meet the following requirements:
+            <ul>
+              <li>Minimum length of 6 characters</li>
+              <li>At least 1 uppercase character</li>
+              <li>At least 1 lowercase character</li>
+              <li>At least 1 number</li>
+              <li>{`At least 1 special character (!@#$%^&*()_-+={[}]|:;"'<,>.)`}</li>
+            </ul>
+          </div>
+          <form>
+            <PasswordInput
+              name="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <PasswordInput
+              name="confirmPassword"
+              placeholder="Confirm Password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+          </form>
+          {!isMatching && <p>The passwords do not match.</p>}
+          <button
+            disabled={!isMatching || !confirmPassword}
+            onClick={validatePassword}
+          >
+            Submit
+          </button>
+        </>
+      )}
     </div>
   );
 };
